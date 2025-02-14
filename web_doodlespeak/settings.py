4cce97801b1be649ecd0998af6f1db4e3146622d
Rogerio Soares <gerasoa@gmail.com>
2025-02-14 11:59:57 +0000

    Update ALLOWED_HOSTS to include Heroku domains

--- web_doodlespeak/settings.py
+++ web_doodlespeak/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = [
+                '127.0.0.1',
+                '*.herokuapp.com',
+]
+
 
 CSRF_TRUSTED_ORIGINS = [
     "https://*.codeinstitute-ide.net/",
