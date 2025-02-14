8917b75ea48803613639633e5773f4a1e0a0d281
Shari Chung <sharichungdesign@gmail.com>
2025-02-14 04:05:46 +0000

    add templates, favicon and css

--- web_doodlespeak/profiles/views.py
+++ web_doodlespeak/profiles/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from datetime import datetime
 
 # Create your views here.
+
+
 def index(request):
-    return HttpResponse("Hello, world. You're at the profiles index.")
+    # return HttpResponse("Hello, world. You're at the profiles index.")
+    return render(request, 'profiles/index.html')
+
+
+def index(request):
+    current_year = datetime.now().year
+    return render(request, 'profiles/index.html', {'current_year': current_year})
--- web_doodlespeak/web_doodlespeak/settings.py
+++ web_doodlespeak/web_doodlespeak/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0x$#y=be$q40k!coepo6+(yrb$cr=al)(0nhas6k)a$tj)4w2i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -60,7 +60,7 @@ ROOT_URLCONF = 'web_doodlespeak.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,7 +126,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
