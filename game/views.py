b88a3e1ba8e5d769614a0e56caff3dd2f9b4cc04
Rogerio Soares <gerasoa@gmail.com>
2025-02-17 16:23:22 +0000

    Add Google visio

--- game/views.py
+++ game/views.py
@@ -29,6 +29,8 @@ def add_word(request):
             return redirect('dashboard_index')
     return render(request, 'game/game.html')
 
+def index(request):
+    return render(request, 'game/index.html')
 
 @login_required
 def analyze_image(request):
@@ -36,18 +38,11 @@ def analyze_image(request):
     if request.method == 'POST':
         data = json.loads(request.body)
         image_data = data['image'].split(",")[1]
-        print(f"Image data length: {len(image_data)}")
+        target_word = data['target_word']
+        print(f"Image data length: {target_word}")
         image = Image.open(BytesIO(base64.b64decode(image_data)))
         buffered = BytesIO()
         image.save(buffered, format="PNG")
-
-
-        # Save the image to a temporary file
-        # temp_image_path = os.path.join(os.path.dirname(__file__), 'temp_image.png')
-        # with open(temp_image_path, 'wb') as f:
-        #     f.write(buffered.getvalue())
-        # image.show() 
-
 
         client = vision.ImageAnnotatorClient()
         content = buffered.getvalue()
@@ -56,5 +51,39 @@ def analyze_image(request):
         response = client.label_detection(image=image)
         labels = response.label_annotations
         print(f"API Response: {response}")
-        return JsonResponse({'labels': [label.description for label in labels]})
+
+        # Comparar as labels retornadas com a palavra alvo
+        target_words = target_word.lower().split()
+        matched_labels = [label.description for label in labels if any(word in label.description.lower().split() for word in target_words)]
+
+        return JsonResponse({'labels': matched_labels})
     return JsonResponse({'error': 'Invalid request'}, status=400)
+
+# @login_required
+# def analyze_image(request):
+#     print("Analyzing image")
+#     if request.method == 'POST':
+#         data = json.loads(request.body)
+#         image_data = data['image'].split(",")[1]
+#         print(f"Image data length: {len(image_data)}")
+#         image = Image.open(BytesIO(base64.b64decode(image_data)))
+#         buffered = BytesIO()
+#         image.save(buffered, format="PNG")
+
+
+#         # Save the image to a temporary file
+#         # temp_image_path = os.path.join(os.path.dirname(__file__), 'temp_image.png')
+#         # with open(temp_image_path, 'wb') as f:
+#         #     f.write(buffered.getvalue())
+#         # image.show() 
+
+
+#         client = vision.ImageAnnotatorClient()
+#         content = buffered.getvalue()
+#         print(f"Image content length: {len(content)}")
+#         image = vision.Image(content=content)
+#         response = client.label_detection(image=image)
+#         labels = response.label_annotations
+#         print(f"API Response: {response}")
+#         return JsonResponse({'labels': [label.description for label in labels]})
+#     return JsonResponse({'error': 'Invalid request'}, status=400)
